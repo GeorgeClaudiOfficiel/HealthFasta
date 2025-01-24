fa1390be9bd4952192491506f68aba77d49bf21e
GeorgeClaudiOfficiel <georgeclaudiofficiel@gmail.com>
2025-01-25 00:20:24 +0300

    Update database connection details in config.py

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'Claudio17.'
+    MYSQL_DB = 'healthfasta'
+    SECRET_KEY = 'e9454e10d40412fe95097b3a272474bd3d6b809ae0d99213'
+    CORS_HEADERS = 'Content-Type'
